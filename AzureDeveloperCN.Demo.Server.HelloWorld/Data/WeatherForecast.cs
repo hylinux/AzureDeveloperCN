16c5fc61e05c31d854e980a56ad33ee08f0bcfb4
HongWei Guo <hongweig@163.com>
2021-02-21 20:18:35 +0800

    添加了一个新的基于Blazor Server模板的Demo项目

---
+++ AzureDeveloperCN.Demo.Server.HelloWorld/Data/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace AzureDeveloperCN.Demo.Server.HelloWorld.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
